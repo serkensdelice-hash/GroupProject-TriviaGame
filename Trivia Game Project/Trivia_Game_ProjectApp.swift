162857125cdd997a44157fc0271ce2bbdd505535
Serkens Delice <sdeli022@fiu.edu>
2026-04-04 16:06:00 -0400

    Initial Commit

---
+++ Trivia Game Project/Trivia_Game_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_Game_ProjectApp.swift
+//  Trivia Game Project
+//
+//  Created by Serkens Delice on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_Game_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
